5dbc51daa77a488eccce15960fbe041cb9825774
terracota17 <80950832+terracota17@users.noreply.github.com>
2024-05-21 23:11:38 +0200

    Delete app directory

--- app/assets/fonts/fonts.py
+++
@@ -1,4 +0,0 @@
-import tkinter.font as tkFont
-
-def get_arial20():
-    return tkFont.Font(family="Arial", size=18, weight="bold")
--- app/main.py
+++
@@ -1,5 +0,0 @@
-from app.gui import App
-
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
