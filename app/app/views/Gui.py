ed7d41113654e067f0a63e8d0e54cef798530fcd
terracota17 <80950832+terracota17@users.noreply.github.com>
2024-06-02 10:20:17 +0200

    Update Gui.py

--- app/app/views/Gui.py
+++ app/app/views/Gui.py
@@ -256,7 +256,5 @@ class App(tk.Tk):
 
         #retrieve data from api
         dates = self.get_dates_in_month(datetime.today().month)
-    
-        #self.controller.fitbitAPI.getUserProfile()
         self.controller.fitbitAPI.getHeartRateData("1min", "00:00", "23:59", dates)
         self.controller.fitbitAPI.getCaloriesDistanceStepsData("1min", "00:00", "23:59", dates)    
