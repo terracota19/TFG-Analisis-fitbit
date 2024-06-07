ccb10667fcdf6c47ed2a3e5437e91bac59f6c02a
terracota17 <80950832+terracota17@users.noreply.github.com>
2024-06-07 09:38:07 +0200

    Update UserFitbitAccountAlreadyInUseError.py

--- app/app/exceptions/UserFitbitAccountAlreadyInUseError.py
+++ app/app/exceptions/UserFitbitAccountAlreadyInUseError.py
@@ -1,7 +1,7 @@
 # UserFitbitAccountAlreadyInUseError.py
 
 class UserFitbitAccountAlreadyInUseError(Exception):
-    """Excepción personalizada para errores en el uso de autorizacion con una cuenta de fitbit existe de otro usuario."""
+    """Excepción personalizada para la prevención de creación de dos cuentas enlazadas con el mismo correo de fitbit."""
     def __init__(self, message):
         super().__init__(message)
         self.message = message
