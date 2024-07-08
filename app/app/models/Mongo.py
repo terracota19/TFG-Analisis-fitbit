ad9c1c45496e7283b0b69cd1c78acffa75e61b2c
terracota17 <80950832+terracota17@users.noreply.github.com>
2024-07-08 23:18:20 +0200

    Updated app, documented Controller.py

---
+++ app/app/models/Mongo.py
@@ -0,0 +1,34 @@
+class Mongo:
+    def __init__(self, dbname, mongoClient):
+        
+        """Mongo attributtes"""
+        self.client = mongoClient
+        self.db = self.client[dbname]
+        
+    """Close connection with MongoDB"""
+    def close_connection(self):
+        self.client.close()
+
+    """CRUD Operations"""
+    def insert_data(self, collection_name, data):
+        collection = self.db[collection_name]
+        result = collection.insert_one(data)
+        return result.acknowledged
+
+    def find_data(self, collection_name, query):
+        collection = self.db[collection_name]
+        return collection.find(query)
+
+    def find_one_data(self, collection_name, query):
+        collection = self.db[collection_name]
+        return collection.find_one(query)
+    
+    def update_data(self, collection_name, query, field, value):
+        collection = self.db[collection_name]
+        result = collection.update_one(query, {"$set": {field: value}})
+        return result.modified_count
+
+    def delete_data(self, collection_name, query):
+        collection = self.db[collection_name]
+        result = collection.delete_one(query)
+        return result.deleted_count
