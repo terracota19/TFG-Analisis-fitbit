b0842df6ad62190f515d1f06143a7e6a2222bc65
terracota17 <80950832+terracota17@users.noreply.github.com>
2024-07-10 19:52:51 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,12 +2,12 @@ from app.views.Gui import App
 
 if __name__ == "__main__":
     """
-        App HeartPred'it instance
+        App HeartPred'it instance.
     """
     app = App()
     
     """
-        Logic for closing mongoDB client conection and stop localhost OAuthServer on port 5000 and destroy app HeartPred'it
+        Logic for closing mongoDB client conection and stop localhost OAuthServer on port 5000 and destroy app HeartPred'it.
     """
     def on_close():
         
@@ -18,7 +18,9 @@ if __name__ == "__main__":
         app.controller.oauth_server.stop_server()
 
         app.destroy()
-
+    """
+        Associate closing app window with on_close logic.
+    """
     app.protocol("WM_DELETE_WINDOW", on_close)
 
     app.mainloop()
