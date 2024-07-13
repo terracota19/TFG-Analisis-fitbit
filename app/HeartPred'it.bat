1186269540509ae0b742b2439ab071ba662f0eaa
terracota17 <80950832+terracota17@users.noreply.github.com>
2024-07-13 23:31:10 +0200

    Update HeartPred'it.bat

--- app/HeartPred'it.bat
+++ app/HeartPred'it.bat
@@ -1,28 +1,28 @@
 @echo off
 
-REM 
+REM GLOBAL VARIABLES
 SET MONGO_PATH="C:\Program Files\MongoDB\Server\7.0\bin\mongod.exe"
 SET DB_PATH="C:\hlocal\datos"
 
-REM 
+REM CHECK WHETHER %DB_PATH% exists or not, and creating if it does not exist
 IF NOT EXIST %DB_PATH% (
     echo Creating directory %DB_PATH%...
     mkdir %DB_PATH%
 )
 
-REM 
+REM Check pip version and ensuring localhost has it, in order to install HeartPred'it dependancies
 python -m pip --version
 IF %ERRORLEVEL% NEQ 0 (
     echo Installing pip...
     python -m ensurepip --default-pip
 )
 
-REM
-echo Installing dependencies...
+REM Installing HeartPred'it dependencias
+echo Installing HeartPred'it dependencies...
 pip install -r requirements.txt
 
-REM 
+REM Start MongoDB on %DB_PATH%
 start "" /B %MONGO_PATH% --dbpath %DB_PATH%
 
-REM 
+REM Start HeartPred'it App
 start "" "python.exe" "main.py"
