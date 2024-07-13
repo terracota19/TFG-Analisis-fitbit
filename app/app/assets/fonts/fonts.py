a9a1947389e1a43a8c1fb3a9e676762220856fa3
terracota17 <80950832+terracota17@users.noreply.github.com>
2024-07-13 22:53:36 +0200

    Update fonts.py

--- app/app/assets/fonts/fonts.py
+++ app/app/assets/fonts/fonts.py
@@ -1,5 +1,7 @@
-import tkinter as tk
 from tkinter import font as tkFont
 
+""""
+    Getter for tk.Font (Arial, 20)
+"""
 def get_arial20():
     return tkFont.Font(family="Arial", size=18, weight="bold")
