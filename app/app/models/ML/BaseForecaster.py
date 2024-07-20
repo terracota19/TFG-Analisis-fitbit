bb4baa4eea0b84d34a3d68e4e13800b9feb7426d
terracota17 <80950832+terracota17@users.noreply.github.com>
2024-07-20 09:49:08 +0200

    Updated app

---
+++ app/app/models/ML/BaseForecaster.py
@@ -0,0 +1,56 @@
+
+from skforecast.ForecasterAutoreg import ForecasterAutoreg
+
+class BaseForecaster :
+
+    def __init__(self, model):
+
+        "Forecaster Information"
+        self.steps = None
+        self.lags =  None
+
+        self.datos_train = None
+        self.future_exog = None
+        
+        """ML model"""
+        self.model = model
+        
+        """Forecaster AutoReg"""
+        self.forecaster = None 
+    
+    """
+        Getter for HearRate User Data.
+    """
+    def datosReales(self):
+        return self.datos_train['HeartRate'][-self.steps:]
+        
+    """
+        Predice en el futuro para dentro de "minutes" minutos
+    """
+    def predictions(self):
+        return self.forecast()
+    
+    """
+        Entrenamos el modelo con los datos de entrenamiento y variables exogenenas
+    """
+    def fitModel(self, datos_train, future_exog, steps, best_lags):
+
+        self.steps = steps
+        self.lags = best_lags
+
+        self.forecaster = ForecasterAutoreg(
+            regressor = self.model,
+            lags      = best_lags
+        )
+        
+        self.datos_train = datos_train        
+        self.future_exog = future_exog
+        
+        self.forecaster.fit(y=self.datos_train['HeartRate'], exog=self.datos_train[['Calories','Steps','Distance']])
+       
+    """
+        Para predecir a x minutos en el futuro
+    """
+    def forecast(self):
+        self.predicciones = self.forecaster.predict(steps=self.steps, exog=self.future_exog)
+        return self.predicciones
