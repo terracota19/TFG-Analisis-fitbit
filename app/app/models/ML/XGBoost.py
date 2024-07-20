6e8537f6ec141fa7ea49611e3bfa60d6c5b6badc
terracota17 <80950832+terracota17@users.noreply.github.com>
2024-07-20 20:55:15 +0200

    Delete app/app directory

--- app/app/models/ML/XGBoost.py
+++
@@ -1,9 +0,0 @@
-from xgboost import XGBRegressor
-from app.models.ML.BaseForecaster import BaseForecaster
-
-class XGBoost(BaseForecaster) :
-
-    def __init__(self):
-        self.model = XGBRegressor(random_state = 123)
-        super().__init__(self.model)
-       
