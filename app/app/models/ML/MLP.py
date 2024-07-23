e1249c08417097fb7358b7d033497c510395d7a6
terracota17 <80950832+terracota17@users.noreply.github.com>
2024-07-23 22:39:57 +0200

    Delete app/app directory

--- app/app/models/ML/MLP.py
+++
@@ -1,9 +0,0 @@
-from sklearn.neural_network import MLPRegressor
-from app.models.ML.BaseForecaster import BaseForecaster
-
-class MLP (BaseForecaster):
-
-    def __init__(self):
-        self.model = MLPRegressor(random_state=123)
-        super().__init__(self.model)
-       
