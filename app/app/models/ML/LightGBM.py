276c3afc42962c1d46d0b25164619324cb5b8219
terracota17 <80950832+terracota17@users.noreply.github.com>
2024-07-24 17:06:33 +0200

    Delete app/app directory

--- app/app/models/ML/LightGBM.py
+++
@@ -1,9 +0,0 @@
-import lightgbm as lgb
-from app.models.ML.BaseForecaster import BaseForecaster
-
-class LightGBM (BaseForecaster) :
-
-    def __init__(self):
-        self.model = lgb.LGBMRegressor(random_state=123)
-        super().__init__(self.model)
-      
