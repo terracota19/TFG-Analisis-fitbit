20ffcc5a83f3e9e67703dfb07717750979d10228
terracota17 <80950832+terracota17@users.noreply.github.com>
2024-07-30 10:12:50 +0200

    Update HeartPred'it.bat

--- app/HeartPred'it.bat
+++ app/HeartPred'it.bat
@@ -10,19 +10,19 @@ IF NOT EXIST %DB_PATH% (
     mkdir %DB_PATH%
 )
 
-REM Check pip version and ensuring localhost has it, in order to install HeartPred'it dependancies
+REM Check pip version and ensuring localhost has it, in order to install HeartPred'it dependancies.
 python -m pip --version
 IF %ERRORLEVEL% NEQ 0 (
     echo Installing pip...
     python -m ensurepip --default-pip
 )
 
-REM Installing HeartPred'it dependencies
+REM Installing HeartPred'it dependencies.
 echo Installing HeartPred'it dependencies...
 pip install -r requirements.txt
 
-REM Start MongoDB on %DB_PATH%
+REM Start MongoDB on %DB_PATH%.
 start "" /B %MONGO_PATH% --dbpath %DB_PATH%
 
-REM Start HeartPred'it App
+REM Start HeartPred'it App.
 start "" "python.exe" "main.py"
