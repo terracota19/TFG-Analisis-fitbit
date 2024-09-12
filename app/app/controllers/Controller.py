e9f112e54fe86e05de47b0b51f17b58bcc8c67d3
terracota17 <80950832+terracota17@users.noreply.github.com>
2024-09-12 13:04:44 +0200

    Update Controller.py

--- app/app/controllers/Controller.py
+++ app/app/controllers/Controller.py
@@ -41,8 +41,8 @@ class Controller:
         javi_fitbit_client_secret = os.getenv("JAVI_FITBIT_CLIENT_SECRET")
 
     
-        #self.fitbitAPI = FitbitAPI(juan_fitbit_client_id, juan_fitbit_client_secret, self.mongo)
-        self.fitbitAPI = FitbitAPI(javi_fitbit_client_id, javi_fitbit_client_secret, self.mongo)
+        self.fitbitAPI = FitbitAPI(juan_fitbit_client_id, juan_fitbit_client_secret, self.mongo)
+        #self.fitbitAPI = FitbitAPI(javi_fitbit_client_id, javi_fitbit_client_secret, self.mongo)
     
         """OAuthServer""" 
         self.oauth_server = OAuthServer(self.fitbitAPI)
